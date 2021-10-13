097eabe53e7ab695d3ae044d983e5b43ec39e7f3
Iván Cabrera <hec_ivan_150@hotmail.com>
2021-10-12 23:41:01 -0500

    Feature: Add account menu view

--- imports/ui/layouts/shared/HeaderView.tsx
+++ imports/ui/layouts/shared/HeaderView.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
-import { IconButton, Theme, Toolbar } from '@mui/material';
+import { Box, IconButton, Theme, Toolbar } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import MenuIcon from '@mui/icons-material/Menu';
 import { DRAWER_WIDTH } from '/imports/ui/layouts/shared/NavigationDrawer';
@@ -8,6 +8,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setDrawerAction } from '/imports/ui/modules/temporal';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import Breadcrumb from '/imports/ui/layouts/shared/Breadcrumb';
+import UserLogged from '/imports/ui/components/UserLogged/UserLogged';
 
 interface AppBarProps extends MuiAppBarProps {
 	open?: boolean;
@@ -51,6 +52,8 @@ const HeaderView = () => {
 					<MenuIcon/>
 				</IconButton>
 				<Breadcrumb/>
+				<Box component="div" sx={ { flexGrow: 1 } }/>
+				<UserLogged/>
 			</Toolbar>
 		</AppBar>
 	);
