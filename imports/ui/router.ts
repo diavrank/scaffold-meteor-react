099a4a56d31000c1223a203dbbc51ff10c7494b6
Iván Cabrera <hec_ivan_150@hotmail.com>
2021-10-18 16:11:28 -0500

    Fix: Complete routing configuration for the current routes

--- imports/ui/router.ts
+++ imports/ui/router.ts
@@ -1,5 +1,5 @@
 import ReactViewRouter from 'react-view-router';
 
-const router = new ReactViewRouter();
+const router = new ReactViewRouter({ mode: 'browser' });
 
 export default router;
--- imports/ui/routes/routes.ts
+++ imports/ui/routes/routes.ts
@@ -3,27 +3,95 @@ import LytSpa from '/imports/ui/layouts/LytSPA';
 import LytAuth from '/imports/ui/layouts/LytAuth';
 import Login from '/imports/ui/views/Auth/Login';
 import ForgotPassword from '/imports/ui/views/Auth/ForgotPassword';
+import ResetPassword from '/imports/ui/views/Auth/ResetPassword';
+import VerifyAccount from '../views/Auth/VerifyAccount';
+import SetInitialPassword from '/imports/ui/views/Auth/SetInitialPassword';
+import ListUsers from '/imports/ui/views/Users/ListUsers';
+import Home from '/imports/ui/views/Home/Home';
+import SaveUser from '/imports/ui/views/Users/SaveUser';
+import AnonymousView from '/imports/ui/routes/AnonymousView';
 
 export default normalizeRoutes([
-	{ path: '/', index: 'home' },
 	{
 		path: '/home',
-		component: LytSpa
+		component: LytSpa,
+		children: [
+			{
+				name: 'home',
+				path: '/',
+				exact: true,
+				components: {
+					sectionView: Home
+				}
+			},
+			{
+				path: '/users',
+				components: {
+					sectionView: AnonymousView
+				},
+				children: [
+					{
+						name: 'home.users',
+						path: '/',
+						exact: true,
+						component: ListUsers
+					},
+					{
+						name: 'home.users.create',
+						path: '/create',
+						component: SaveUser
+					},
+					{
+						name: 'home.users.edit',
+						path: '/edit',
+						component: SaveUser
+					}
+				]
+			}
+		]
 	},
 	{
-		path: '/login',
+		path: '/',
+		redirect: '/login'
+	},
+	{
+		path: '/',
 		component: LytAuth,
 		children: [
 			{
+				name: 'login',
+				path: 'login',
+				exact: true,
+				components: {
+					sectionView: Login
+				}
+			},
+			{
+				name: 'forgotPassword',
 				path: '/forgot-password',
 				components: {
 					sectionView: ForgotPassword
 				}
 			},
 			{
-				path: '/',
+				name: 'resetPassword',
+				path: '/reset-password',
 				components: {
-					sectionView: Login
+					sectionView: ResetPassword
+				}
+			},
+			{
+				name: 'verifyAccount',
+				path: '/verify-email',
+				components: {
+					sectionView: VerifyAccount
+				}
+			},
+			{
+				name: 'enrollAccount',
+				path: '/enroll-account',
+				components: {
+					sectionView: SetInitialPassword
 				}
 			}
 		]
